eec8e39f8fa74fb3323db5cd44f51d3ad8875c1c
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-11-09 18:46:35 +0200

    +++

---
+++ src/js/menu.js
@@ -0,0 +1,40 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+
+
+
+jQuery(document).ready(function($) {
+  "use strict";
+  $("#testimonials-list").owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1200: {
+        items: 3
+      }
+    }
+  });
+});
